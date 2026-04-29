60464bf01e0b4757642129a4591bd068e671b694
d3-fau1t <hkpanwar123@gmail.com>
2026-04-29 12:16:57 +0530

    added auth and base for user suth sys and saving and hashing pass

--- app/auth.py
+++ app/auth.py
@@ -1,24 +1,56 @@
-from datetime import datetime, timedelta
-from jose import jwt
-from passlib.context import CryptContext
+#auth system for pass and login in authentication system
 
-SECRET_KEY = ""
+from datetime  import datetime, timedelta , timezone
+from jose import JWTError , jwt
+from passlib.context import CryptoContext
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app.database import get_db
+
+#using temp key for now
+
+SECRET_KEY = "vibematch-indev"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = 60*24
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+pwd_context = CryptoContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str):
+#fist for hashing and verfiing password
+
+def hash_password(password:str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(plain,hashed):
-    return pwd_context.verify(plain, hashed)
+def verify_password(plain:str , hashed: str) -> bool:
+    return pwd_context.verify(plain , hashed)
 
-def create_access_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp":expire})
-    return jwt.encode(to_encode, SECRET_KEY,algorithm=ALGORITHM)
+def create_access_token(user_id: int) -> str:
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    payload = {"sub" : str(user_id), "exp" : expire }
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str) -> int:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        return int(user_id)
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token or expired")
+    
+    oauth2_secheme = OAuth2PasswordBearer(tokenUrl="/login")
+
+    def get_current_user(
+            token: str = Depends(oauth2_secheme),
+            db: Session = Depends(get_db)
 
 
+    ):
+        from app.models.user import User
+        user_id = decode_access_token(token)
+        user = db.query(User).filter(User.id == user_id).first()
 
+        if not user:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+        return user
