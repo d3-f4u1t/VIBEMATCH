1567a7fc1d8234a4ba15018dfb4d681d937584f2
d3-fau1t <hkpanwar123@gmail.com>
2026-05-01 18:20:01 +0530

    fixed wrong code str

--- app/auth.py
+++ app/auth.py
@@ -39,18 +39,15 @@ def decode_access_token(token: str) -> int:
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token or expired")
     
-    oauth2_secheme = OAuth2PasswordBearer(tokenUrl="/login")
+oauth2_secheme = OAuth2PasswordBearer(tokenUrl="/login")
 
-    def get_current_user(
-            token: str = Depends(oauth2_secheme),
-            db: Session = Depends(get_db)
-
-
-    ):
-        from app.models.user import User
-        user_id = decode_access_token(token)
-        user = db.query(User).filter(User.id == user_id).first()
-
-        if not user:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
-        return user
+def get_current_user(
+        token: str = Depends(oauth2_secheme),
+        db: Session = Depends(get_db)
+):
+    from app.models.user import User
+    user_id = decode_access_token(token)
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    return user
